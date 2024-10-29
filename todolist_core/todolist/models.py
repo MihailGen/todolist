2d4c4d768c00188f1ca34f8b563e9a9999a492c7
Mihail Genzik <genziks@inbox.lv>
2024-10-29 18:47:26 +0200

    V2

--- todolist_core/todolist/models.py
+++ todolist_core/todolist/models.py
@@ -6,7 +6,23 @@ class Task(models.Model):
     due_date = models.DateTimeField(null=True, blank=True, verbose_name='Cроки выполнения')
     tags = models.TextField(default='задачи', verbose_name='Теги')
     author = models.CharField(max_length=50, null=True, blank=True, verbose_name='Автор')
+    tags = models.ManyToManyField('Tag', related_name='tasks')
     class Meta:
         verbose_name = 'задача'
         verbose_name_plural = 'задачи'
 
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.id} on {self.created_at}'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
